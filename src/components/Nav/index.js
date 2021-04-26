b284dc6dc8a7e5271a4d771761b6248513a6e1fd
AdamR-Work <aramosatwork@hotmail.com>
2021-04-26 16:04:17 -0500

    started gallery

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(){
     const categories = [
@@ -37,7 +38,7 @@ function Nav(){
                     {categories.map((category)=>(
                         <li className="mx-1" key={category.name}>
                             <span onClick={()=>categorySelected(category.name)}>
-                                {category.name}
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                          </li>
                     ))}
